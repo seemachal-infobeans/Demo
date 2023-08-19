23b1004b4328df9cd4b88d0176709158fb7520a1
Seemachal <seemachal.goud@infobeans.com>
2023-08-19 20:41:16 +0530

    user data showed in ui and delete from local storage

--- form.js
+++ form.js
@@ -1,3 +1,27 @@
+function displayUserData(userData) {
+    var list = document.getElementById("submissionList");
+    list.innerHTML = ''; // Clear the list
+  
+    userData.forEach(function(data, index) {
+      var listItem = document.createElement("li");
+      listItem.className = "list-group-item d-flex justify-content-between align-items-center";
+      listItem.innerHTML = data.name + " - " + data.email + " - " + data.mobile;
+      
+      var deleteButton = document.createElement("button");
+      deleteButton.className = "btn btn-danger btn-sm";
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", function() {
+        userData.splice(index, 1); // Remove the submission from the array
+        localStorage.setItem("formData", JSON.stringify(userData));
+        displayUserData(userData); // Re-render the list
+      });
+      
+      listItem.appendChild(deleteButton);
+      list.appendChild(listItem);
+    });
+  }
+
+
 document.getElementById("myForm").addEventListener("submit", function(event) {
     event.preventDefault();
   
@@ -28,6 +52,6 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
     localStorage.setItem("formData", JSON.stringify(existingData));
   
     document.getElementById("myForm").reset();
-  
+    displayUserData(existingData);
     alert("Form data has been stored in local storage.");
 })
