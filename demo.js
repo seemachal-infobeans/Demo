29c933040bfd88fd07ffa43cec100d651359c1ba
Seemachal <seemachal.goud@infobeans.com>
2023-08-19 11:00:20 +0530

    getElemntsByTagName

--- demo.js
+++ demo.js
@@ -1,5 +1,18 @@
+//by Class Name
 const item = document.getElementsByClassName("list-group-item");
 for (let i = 0; i < item.length; i++) {
     item[i].style.fontWeight = "bold";
   }
 item[2].style.backgroundColor ="green";
+item[2].textContent ="item changed";
+
+
+//By Tag Name
+
+
+const li =  document.getElementsByTagName('li');
+
+   li[4].style.fontWeight="bold";
+ 
+
+ 
