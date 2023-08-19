3ff871961cec4267245bca3525cbd09e62b9a443
Seemachal <seemachal.goud@infobeans.com>
2023-08-19 12:41:16 +0530

    Creating Nodes and Modifying Dom

--- demo.js
+++ demo.js
@@ -27,6 +27,56 @@ const li =  document.getElementsByTagName('li');
     
   }
 
+  //Creating Nodes and Modifying Dom
+
+  var itemList = document.querySelector('#items');
+  //parentNode
+  //itemList.parentNode.style.backgroundColor="#f4f4f4";
+   //parentElement
+   itemList.parentElement.style.backgroundColor="#f4f4f4"
+
+   //childNode
+   itemList.children[0].style.backgroundColor='blue';
+
+   //FirstElementChild
+   itemList.firstElementChild.textContent="Hello first";
+
+   //lastelementchild
+
+   itemList.lastElementChild.textContent="hello last";
+
+   //nextElementSibling
+   itemList.nextElementSibling.style.fontWeight="bold";
+
+   //previousElementSibling
+   itemList.previousElementSibling.style.color="lightblue";
+
+   //crateElement
+
+   //create div
+
+   var newdiv = document.createElement('div');
+   //add class
+   newdiv.className="hello";
+
+   //add id
+   newdiv.id = "hello1";
+
+   //add attribute
+   newdiv.setAttribute("title","hello div");
+
+   //create text node
+   var newDivtext = document.createTextNode("hello world");
+
+   //add to div
+   newdiv.appendChild(newDivtext);
+var container = document.querySelector('header .container')
+   var h1 = document.querySelector('header h1');
+
+   container.insertBefore(newdiv,h1)
+
+   
+
  
 
  
